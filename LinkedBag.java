48c546c28892881b37610e3db986aa8c7ab4757c
Ryan Larson <rwlarson@cpp.edu>
2022-02-22 17:56:20 -0800

    Moved the Node.java file into the LinkedBag.java class and used ResizablebBagArrayTest.java to write LinkedBagTest.java

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,7 +1,7 @@
 //Initial commit 
 
 
-package BagPackage;
+
 public class LinkedBag<T> implements BagInterface<T> 
 {
     private Node<T> firstNode;
@@ -14,7 +14,33 @@ public class LinkedBag<T> implements BagInterface<T>
     }
     private class Node
     {
-
+        private T data;
+        private Node next;
+        private Node(T dataPortion)
+        {
+            this(dataPortion, null);
+        }
+        private Node(T dataPortion, Node nextNode)
+        {
+            data = dataPortion;
+            next = nextNode;
+        }
+        private T getData()
+        {
+            return data;
+        }
+        private void setData(T newData)
+        {
+            data = newData;
+        }
+        private Node getNextNode()
+        {
+            return next;
+        }
+        private void setNextNode(Node nextNode)
+        {
+            next = nextNode;
+        }
     }
     public boolean add(T newEntry)
     {
