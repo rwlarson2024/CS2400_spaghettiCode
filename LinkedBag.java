d18253cd27c4355e10b26de825a43ee947da21b8
Estevan Delgado <estevand@cpp.edu>
2022-02-23 13:44:47 -0800

    testing

---
+++ LinkedBag.java
@@ -0,0 +1,298 @@
+//Initial commit 
+//Can this code work for me?
+//testing
+public class LinkedBag<T> implements BagInterface<T> 
+{
+    private Node<T> firstNode;
+    private int numberOfEntries;
+    private boolean integrityOK = false;
+
+    public LinkedBag()
+    {
+        firstNode = null;
+        numberOfEntries = 0;
+        integrityOK = true;
+    }
+
+    public LinkedBag(LinkedBag<T> other)
+    {
+        this.integrityOK = other.integrityOK;
+        Node<T> currentNode = other.firstNode;
+        while (currentNode != null)
+        {
+            this.add(currentNode.getData());
+            currentNode = currentNode.getNextNode();
+        }
+        this.numberOfEntries = other.numberOfEntries;
+    }
+
+    private class Node<T>
+    {
+        private T data;
+        private Node<T> next;
+        Node(T dataPortion)
+        {
+            this(dataPortion, null);
+        }
+        Node(T dataPortion, Node<T> nextNode)
+        {
+            data = dataPortion;
+            next = nextNode;
+        }
+        T getData()
+        {
+            return data;
+        }
+        void setData(T newData)
+        {
+            data = newData;
+        }
+        Node<T> getNextNode()
+        {
+            return next;
+        }
+        void setNextNode(Node<T> nextNode)
+        {
+            next = nextNode;
+        }
+    }
+
+    public boolean add(T newEntry)
+    {
+        checkIntegrity();
+        boolean result = true;
+        Node<T> newNode = new Node<>(newEntry);
+        newNode.next = firstNode;
+
+        firstNode = newNode;
+        numberOfEntries++; 
+        return result;
+    }
+
+    public boolean add(T[] contents)
+    {
+        checkIntegrity();
+        boolean result = true;
+        for (int index = 0; index < contents.length; index++)
+        {
+            add(contents[index]);
+        }
+        return result;
+    }
+
+    public T remove()
+    {
+        checkIntegrity();
+        T result = null;
+        if (firstNode != null)
+        {
+            result = firstNode.getData();
+            firstNode = firstNode.getNextNode();
+            numberOfEntries--;
+        }
+        return result; 
+    }
+
+    public boolean remove(T anEntry)
+    {
+        checkIntegrity();
+        boolean result = false;
+        Node<T> nodeN = getReferenceTo(anEntry);
+        if (nodeN != null )
+        {
+            nodeN.setData(firstNode.getData());
+            firstNode = firstNode.getNextNode();
+            numberOfEntries--;
+            result = true;  
+        }
+        return result; 
+    }
+
+    private Node<T> getReferenceTo(T anEntry)
+    {
+        boolean found = false;
+        Node<T> currentNode = firstNode;
+        while (!found && (currentNode !=null))
+        {
+            if (anEntry.equals(currentNode.getData()))
+                found = true;
+            else
+                currentNode = currentNode.getNextNode();
+        }
+        return currentNode;
+    }
+
+    public boolean isEmpty()
+    {
+        return numberOfEntries == 0;
+    }
+
+    public int getCurrentSize()
+    {
+        return numberOfEntries;
+    }
+
+    public void clear()
+    {
+        while(!isEmpty())
+            remove();
+    }
+
+    public int getFrequencyOf(T anEntry)
+    {
+        checkIntegrity();
+        int frequency = 0;
+        int counter = 0; 
+        Node<T> currentNode = firstNode;
+        while ((counter < numberOfEntries) && (currentNode != null))
+        {
+            if(anEntry.equals(currentNode.getData()))
+            {
+                frequency++;
+            }
+            counter++;
+            currentNode = currentNode.getNextNode();
+        }
+        return frequency;
+    }
+
+    public boolean contains(T anEntry)
+    {
+        checkIntegrity();
+        boolean found = false;
+        Node<T> currentNode = firstNode;
+        while (!found && (currentNode != null))
+        {
+            if (anEntry.equals(currentNode.getData()))
+                found = true;
+            else
+                currentNode = currentNode.getNextNode();
+        }
+        return found;
+    }
+
+    public T[] toArray()
+    {
+        @SuppressWarnings("unchecked")
+        T[] result = (T[])new Object[numberOfEntries];
+        int index = 0; 
+        Node<T> currentNode = firstNode;
+        while ((index < numberOfEntries) && (currentNode != null))
+        {
+            result[index] = currentNode.getData();
+            index++;
+            currentNode = currentNode.getNextNode();
+        }
+        return result;
+    }
+
+    public void checkIntegrity()
+    {
+        if (!integrityOK)
+            throw new SecurityException("LinkedBag object is corrupt.");
+    }
+
+    public void displayBag()
+    {
+        Object[] temp = this.toArray();
+        for (int index = 0; index < temp.length; index++)
+        {
+            System.out.print(temp[index] + " ");
+        }
+        System.out.println();
+    }
+
+    public BagInterface<T> union(BagInterface<T> other)
+    {
+        LinkedBag<T> tempBag = new LinkedBag<>();
+        /*
+        T[] temp = other.toArray();
+        for (T entry : temp) 
+        {
+            tempBag.add(entry);
+        }
+        temp = this.toArray();
+        for (T entry : temp) 
+        {
+            tempBag.add(entry);
+        }
+        */
+        LinkedBag<T> castedOther = (LinkedBag<T>) other;
+        Node<T> currentNode = castedOther.firstNode;
+        while (currentNode != null)
+        {
+            tempBag.add(currentNode.getData());
+            currentNode = currentNode.getNextNode();
+        }
+        currentNode = this.firstNode;
+        while (currentNode != null)
+        {
+            tempBag.add(currentNode.getData());
+            currentNode = currentNode.getNextNode();
+        }
+        return tempBag;
+
+    }
+
+    public BagInterface<T> intersection(BagInterface<T> other)
+    {
+        BagInterface<T> tempBag = new LinkedBag<>();
+        LinkedBag<T> castedOther = (LinkedBag<T>) other;
+        BagInterface<T> copiedBag = new LinkedBag<>(castedOther);
+        /*
+        T[] temp = this.toArray();
+        for (T entry : temp) 
+        {
+            if (copiedBag.contains(entry))
+            {
+                tempBag.add(entry);
+                copiedBag.remove(entry);
+            }
+        }
+        */
+        Node<T> currentNode = this.firstNode;
+        while (currentNode != null)
+        {
+            if (copiedBag.contains(currentNode.getData()))
+            {
+                tempBag.add(currentNode.getData());
+                copiedBag.remove(currentNode.getData());
+            }
+            currentNode = currentNode.getNextNode();
+        } 
+        return tempBag;
+    }
+
+    public BagInterface<T> difference(BagInterface<T> other)
+    {
+        BagInterface<T> tempBag = new LinkedBag<>();
+        LinkedBag<T> castedOther = (LinkedBag<T>) other;
+        BagInterface<T> copiedBag = new LinkedBag<>(castedOther);
+        /*
+        T[] temp = this.toArray();
+        for (T entry : temp) 
+        {
+            if (copiedBag.contains(entry))
+            {
+                copiedBag.remove(entry);
+                continue;
+            }
+            tempBag.add(entry);
+        }
+        */
+        Node<T> currentNode = this.firstNode;
+        while (currentNode != null)
+        {
+            if (copiedBag.contains(currentNode.getData()))
+            {
+                copiedBag.remove(currentNode.getData());
+            }
+            else
+            {
+                tempBag.add(currentNode.getData());
+            }
+            currentNode = currentNode.getNextNode();
+        } 
+        return tempBag;
+    }
+}
---
+++ ResizableArrayBag.java
@@ -0,0 +1,256 @@
+import java.util.Arrays;
+
+public class ResizableArrayBag<T> implements BagInterface<T>
+{
+    private T[] bag;
+    private static final int DEFAULT_CAPACITY = 25;
+    private static final int MAX_CAPACITY = 10000;
+    private boolean integrityOK = false;
+    private int numberOfEntries;
+
+    public ResizableArrayBag()
+    {
+        numberOfEntries = 0;
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[DEFAULT_CAPACITY];
+        bag = tempBag;
+        integrityOK = true;
+    }
+
+    public ResizableArrayBag(int capacity)
+    {
+        numberOfEntries = 0;
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[capacity];
+        bag = tempBag;
+        integrityOK = true;
+    }
+
+    public ResizableArrayBag(ResizableArrayBag<T> other)
+    {
+        /*
+        @SuppressWarnings("unchecked")
+        T[] tempBag = (T[])new Object[other.bag.length];
+        for (int index = 0; index < other.bag.length; index++)
+        {
+            tempBag[index] = other.bag[index];
+        }
+        */
+        this.bag = Arrays.copyOf(other.toArray(), other.bag.length);;
+        this.integrityOK = other.integrityOK;
+        this.numberOfEntries = other.numberOfEntries;
+    }
+
+    public T[] getBag()
+    {
+        return bag;
+    }
+
+    public boolean getIntegrityOK()
+    {
+        return integrityOK;
+    }
+
+    public int getCurrentSize()
+    {
+        return numberOfEntries;
+    }
+	
+	public boolean isEmpty()
+    {
+        return numberOfEntries == 0;
+    }
+
+    public boolean isFull()
+    {
+        return numberOfEntries == bag.length;
+    }
+	
+	public boolean add(T newEntry)
+    {
+        checkIntegrity();
+        boolean result = true;
+        if(isFull())
+        {
+            doubleCapacity();
+        }
+        bag[numberOfEntries] = newEntry;
+        numberOfEntries++;  
+        return result;
+    }
+
+    public boolean add(T[] contents)
+    {
+        checkIntegrity();
+        boolean result = true;
+        for (int index = 0; index < contents.length; index++)
+        {
+            add(contents[index]);
+        }
+        return result;
+    }
+
+    public void doubleCapacity()
+    {
+        int newLength = 2 * bag.length;
+        checkCapacity(newLength);
+        bag = Arrays.copyOf(bag, newLength);
+    }
+
+    public void checkCapacity(int capacity)
+    {
+        if (capacity > MAX_CAPACITY)
+         throw new IllegalStateException("Attempt to create a bag whose capacity exeeds allowed maximum of " + MAX_CAPACITY);
+    }
+
+	public T remove()
+    {
+        checkIntegrity();
+        T result = removeEntry(numberOfEntries - 1);
+        return result;
+    }
+
+   	public boolean remove(T anEntry)
+    {
+        checkIntegrity();
+        int index = getIndexOf(anEntry);
+        T result = removeEntry(index);         
+        return anEntry == result;
+    }
+
+    public T removeEntry(int givenIndex)
+    {
+        T result = null;
+        if (!isEmpty() && (givenIndex >= 0))
+        {
+            result = bag[givenIndex];
+            bag[givenIndex] = bag[numberOfEntries - 1];
+            bag[numberOfEntries - 1] = null;
+            numberOfEntries--;
+        }
+        return result;
+    }
+
+    public int getIndexOf(T anEntry)
+    {
+        int where = -1;
+        boolean found = false;
+        int index = 0;
+        while (!found && (index < numberOfEntries))
+        {
+            if (anEntry.equals(bag[index]))
+            {
+                found = true;
+                where = index;
+            }
+            index++;
+        } 
+        return where;
+    }
+	
+    public void clear()
+    {
+        while (!isEmpty())
+        {
+            remove();
+        }
+    }
+	
+	public int getFrequencyOf(T anEntry)
+    {
+        checkIntegrity();
+        int counter = 0;
+        for (int index = 0; index < numberOfEntries; index++)
+        {
+            if (anEntry.equals(bag[index]))
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+	
+	public boolean contains(T anEntry)
+    {
+        checkIntegrity();
+        return getIndexOf(anEntry) >= 0;
+    }
+   
+	public T[] toArray()
+    {
+        @SuppressWarnings("unchecked")
+        T[] result = (T[])new Object[numberOfEntries];
+        for (int index = 0; index < numberOfEntries; index++)
+        {
+            result[index] = bag[index];
+        }
+        return result;
+    }
+
+    public void checkIntegrity()
+    {
+      if (!integrityOK)
+         throw new SecurityException("ArrayBag object is corrupt.");
+    }
+
+    public void displayBag()
+    {
+        Object[] temp = this.toArray();
+        for (int index = 0; index < temp.length; index++)
+        {
+            System.out.print(temp[index] + " ");
+        }
+        System.out.println();
+    }
+    
+    public BagInterface<T> union(BagInterface<T> other)
+    {
+        ResizableArrayBag<T> tempBag = new ResizableArrayBag<>();
+        T[] temp = this.toArray();
+        for (T entry : temp) 
+        {
+            tempBag.add(entry);
+        }
+        temp = other.toArray();
+        for (T entry : temp) 
+        {
+            tempBag.add(entry);
+        }
+        return tempBag;
+    }
+    
+    public BagInterface<T> intersection(BagInterface<T> other)
+    {
+        BagInterface<T> tempBag = new ResizableArrayBag<>();
+        ResizableArrayBag<T> castedOther = (ResizableArrayBag<T>) other;
+        BagInterface<T> copiedBag = new ResizableArrayBag<>(castedOther);
+        T[] temp = this.toArray();
+        for (T entry : temp) 
+        {
+            if (copiedBag.contains(entry))
+            {
+                tempBag.add(entry);
+                copiedBag.remove(entry);
+            }
+        }
+        return tempBag;
+    }
+   
+    public BagInterface<T> difference(BagInterface<T> other)
+    {
+        BagInterface<T> tempBag = new ResizableArrayBag<>();
+        ResizableArrayBag<T> castedOther = (ResizableArrayBag<T>) other;
+        BagInterface<T> copiedBag = new ResizableArrayBag<>(castedOther);
+        T[] temp = this.toArray();
+        for (T entry : temp) 
+        {
+            if (copiedBag.contains(entry))
+            {
+                copiedBag.remove(entry);
+                continue;
+            }
+            tempBag.add(entry);
+        }
+        return tempBag;
+    }
+}
