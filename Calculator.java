0d586696a3ba0f835b4910e114d59cc764b3b79c
Allan Chan <allanchan@cpp.edu>
2022-03-14 22:32:06 -0700

    Small changes

--- Calculator.java
+++ Calculator.java
@@ -3,14 +3,17 @@ public class Calculator
     public static void main(String[] args)
     {
         LinkedStack<String> stackOne = new LinkedStack<>();
-        String infix = "a*b/(c-a)+d*e";
+
+        String infix = "a*b/(c-a)+d*e"; // Changeable
+        System.out.println("Infix: " + infix);
 
         String postfix = stackOne.convertToPostfix(infix);
-        System.out.println(postfix); // ab*ca-/de*+
+        System.out.println("Postfix: " + postfix); // ab*ca-/de*+
 
         ResizableArrayStack<String> stackTwo = new ResizableArrayStack<>();
 
+        // a=2 b=3 c=4 d=5 e=6
         int evaluation = stackTwo.evaluatePostfix(postfix);
-        System.out.println(evaluation); // 33
+        System.out.println("Evaluation of postfix: " + evaluation); // 33
     }
 }
