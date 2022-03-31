578ebdb4d69c3a3f3602ab9d7c4569928df8a9fa
Allan Chan <allanchan@cpp.edu>
2022-03-31 14:01:58 -0700

    Small changes

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,6 @@
 // package TreePackage;
 
-public class BinaryTree 
+public class BinaryTree<T> implements BinaryTreeInterface<T>
 {
     
 }
--- EmptyTreeException.java
+++ EmptyTreeException.java
@@ -1,3 +1,5 @@
+// package TreePackage;
+
 public class EmptyTreeException 
 {
     
