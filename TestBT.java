e8d086101824ce0fa67d385a59008e1d1fc37201
Allan Chan <allanchan@cpp.edu>
2022-04-06 00:44:09 -0700

    Sample test case

--- TestBT.java
+++ TestBT.java
@@ -1,27 +1,35 @@
-/*
 import static org.junit.Assert.*;
-
-import javax.naming.BinaryRefAddr;
-
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /** A test of several methods as defined in the BinaryTree class. */
-/*
 public class TestBT
 {
     BinaryTree<String> testTree = new BinaryTree<>();
-    BinaryTree<String> aTree = new BinaryTree<>();
-	
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Test
-    public void testBinaryTree()
+    public void testPostOrderTraverse()
     {
-        createTree1(aTree);
-        assertEquals("a","b","c",root.BinaryTree(rootdata, leftTree, rightTree));
+        createTree1(testTree);
+        System.setOut(new PrintStream(outContent));
+        testTree.postorderTraverse();
+        assertEquals("D\r\nE\r\nB\r\nG\r\nF\r\nC\r\nA", outContent.toString().trim());
     }
-    @Test
-    public void testgetRootData()
-    {
-        assertEquals()
-    }
+
+    public static void createTree1(BinaryTree<String> tree)
+	{ 
+		// Leaves
+		BinaryTree<String> dTree = new BinaryTree<>("D");
+		BinaryTree<String> eTree = new BinaryTree<>("E");
+		BinaryTree<String> gTree = new BinaryTree<>("G");
+
+		// Subtrees:
+		BinaryTree<String> fTree = new BinaryTree<>("F", null, gTree);
+		BinaryTree<String> bTree = new BinaryTree<>("B", dTree, eTree);
+		BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+		tree.setTree("A", bTree, cTree);
+	}
 }
-*/
