366dd9c6200adc06382aff525d16178383e88cc3
rwlarson2024 <98720555+rwlarson2024@users.noreply.github.com>
2022-04-06 15:35:54 -0700

    added new test cases to TestBT

--- TestBT.java
+++ TestBT.java
@@ -9,15 +9,6 @@ public class TestBT
     BinaryTree<String> testTree = new BinaryTree<>();
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @Test
-    public void testPostOrderTraverse()
-    {
-        createTree1(testTree);
-        System.setOut(new PrintStream(outContent));
-        testTree.postorderTraverse();
-        assertEquals("D\r\nE\r\nB\r\nG\r\nF\r\nC\r\nA", outContent.toString().trim());
-    }
-
     public static void createTree1(BinaryTree<String> tree)
 	{ 
 		// Leaves
@@ -32,4 +23,45 @@ public class TestBT
 
 		tree.setTree("A", bTree, cTree);
 	}
+    @Test
+    public void testPostOrderTraverse()
+    {
+        createTree1(testTree);
+        System.setOut(new PrintStream(outContent));
+        testTree.postorderTraverse();
+        assertEquals("D\r\nE\r\nB\r\nG\r\nF\r\nC\r\nA", outContent.toString().trim());
+    }
+
+    @Test
+    public void testPostOrderTraverse_callBinaryNodeMethod()
+    {
+        createTree1(testTree);
+        System.setOut(new PrintStream(outContent));
+        testTree.postorderTraverse_callBinaryNodeMethod();
+        assertEquals("D\r\nE\r\nB\r\nG\r\nF\r\nC\r\nA", outContent.toString().trim());
+    }
+    @Test
+    public void getHeight()
+    {
+        createTree1(testTree);
+        assertEquals(4, testTree.getHeight());
+    }
+    @Test
+    public void getHeight_callBinaryNodeMethod()
+    {
+        createTree1(testTree);
+        assertEquals(4, testTree.getHeight_callBinaryNodeMethod());
+    }
+    @Test
+    public void testgetNumberOfNodes()
+    {
+        createTree1(testTree);
+        assertEquals(7, testTree.getNumberOfNodes());
+    }
+    @Test
+    public void testgetNumberOfNodes_callBinaryNode()
+    {
+        createTree1(testTree);
+        assertEquals(7, testTree.getNumberOfNodes_callBinaryNodeMethod());
+    }
 }
