3cec8faa15641b7e2aa85f13394a29f954b15813
rwlarson2024 <98720555+rwlarson2024@users.noreply.github.com>
2022-04-20 12:27:05 -0700

    Add files via upload
    Made changes to DriverMH but output file is not working correctly

--- DriverMH.java
+++ DriverMH.java
@@ -73,6 +73,73 @@ public class DriverMH
         }
 
         System.out.print("Output printed in output.txt");
+        inputFile.close();
+
+        File fileRand = new File("data_random.txt");
+        inputFile = new Scanner(fileRand);
+
+
+
+        MaxHeap<Integer> heapRand = new MaxHeap<>(100);
+        while (inputFile.hasNext())
+        {
+            heapRand.add(inputFile.nextInt());
+        }
+
+        outputFile.print("Heap built using sequential insertions: ");
+        Integer[] heapRandArray = heapRand.getArray();
+        for (int i = 1; i <= 10; i++)
+        {
+            outputFile.print(heapRandArray[i] + " ");
+        }
+        outputFile.println();
+        outputFile.print("Number of swaps in the heap creation: " + heapRand.getSwapCount());
+        outputFile.println();
+        outputFile.print("Heap after 10 removals: ");
+        for (int i = 1; i <= 10; i++)
+        {
+            heapRand.removeMax();
+        }
+        heapRandArray = heapRand.getArray();
+        for (int i = 1; i <= 10; i++)
+        {
+            outputFile.print(heapRandArray[i] + " ");
+        }
+    
+        inputFile.close();
+        outputFile.println("\n");
+        inputFile = new Scanner(fileRand);
+
+        Integer[] tempRandArray = new Integer[100];
+        int k = 0;
+        while (inputFile.hasNext())
+        {
+            tempRandArray[j] = inputFile.nextInt();
+            k++;
+        }
+        MaxHeap<Integer> heapRandTwo = new MaxHeap<>(tempRandArray);  
+        outputFile.print("Heap built using optimal method: ");
+        Integer[] heapRandTwoArray = heapTwo.getArray();
+        for (int i = 1; i <= 10; i++)
+        {
+            outputFile.print(heapRandTwoArray[i] + " ");
+        }
+        outputFile.println();
+        outputFile.print("Number of swaps in the heap creation: " + heapRandTwo.getSwapCount());
+        outputFile.println();
+        outputFile.print("Heap after 10 removals: ");
+        for (int i = 1; i <= 10; i++)
+        {
+            heapRandTwo.removeMax();
+        }
+        heapRandTwoArray = heapRandTwo.getArray();
+        for (int i = 1; i <= 10; i++)
+        {
+            outputFile.print(heapTwoArray[i] + " ");
+        }
+
+        System.out.print("Output printed in output.txt");
+
 
         inputFile.close();
         outputFile.close();
