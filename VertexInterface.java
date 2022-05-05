d5222e9a6a2dec187be39ae20c3061539ae5e9af
Allan Chan <allanchan@cpp.edu>
2022-05-05 10:24:04 -0700

    Revised methods in vertex classes

--- VertexInterface.java
+++ VertexInterface.java
@@ -62,23 +62,23 @@ public interface VertexInterface<T>
 
    /** Records the previous vertex on a path to this vertex.
        @param predecessor  The vertex previous to this one along a path.  */
-   //public void setPredecessor(VertexInterface<T> predecessor);
+   public void setPredecessor(VertexInterface<T> predecessor);
 
    /** Gets the recorded predecessor of this vertex.
        @return  Either this vertex's predecessor or null if no predecessor
                 was recorded. */
-   //public VertexInterface<T> getPredecessor();
+   public VertexInterface<T> getPredecessor();
 
    /** Sees whether a predecessor was recorded for this vertex.
        @return  True if a predecessor was recorded. */
-   //public boolean hasPredecessor();
+   public boolean hasPredecessor();
 
    /** Records the cost of a path to this vertex.
        @param newCost  The cost of the path. */
-   //public void setCost(double newCost);
+   public void setCost(double newCost);
 
    /** Gets the recorded cost of the path to this vertex.
        @return  The cost of the path. */
-   //public double getCost();
+   public double getCost();
    
 } // end VertexInterface
