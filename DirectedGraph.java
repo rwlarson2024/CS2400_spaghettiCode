29dc6cd4a690fba7cb4acd638b2a1e451d2c5a56
rwlarson2024 <98720555+rwlarson2024@users.noreply.github.com>
2022-05-09 19:36:41 -0700

    Add files via upload

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -1,5 +1,7 @@
 //package GraphPackage;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 //import ADTPackage.*; // Classes that implement various ADTs
 /**
    A class that implements the ADT directed graph.
@@ -9,28 +11,33 @@ import java.util.Iterator;
 */
 public class DirectedGraph<T> implements GraphInterface<T>
 {
-	private DictionaryInterface<T, VertexInterface<T>> vertices;
+	private Map<T, VertexInterface<T>> vertices;
 	private int edgeCount;
+    private boolean[][] edges;
 	
 	public DirectedGraph()
 	{
-		vertices = new SortedLinkedDictionary<T, VertexInterface<T>>();
+		vertices = new LinkedHashMap<>();
 		edgeCount = 0;
+        edges = new boolean[9][9];
 	} // end default constructor
 
     public boolean addVertex(T vertexLabel)
     {
-        VertexInterface<T> addOutcome = vertices.add(vertexLabel, new Vertex<>(vertexLabel));
+        VertexInterface<T> addOutcome = vertices.put(vertexLabel, new Vertex<>(vertexLabel));
         return addOutcome == null;
     }
 
     public boolean addEdge(T begin, T end, double edgeWeight)
     {
         boolean result = false;
-        VertexInterface<T> beginVertex = vertices.getValue(begin);
-        VertexInterface<T> endVertex = vertices.getValue(end);
+        VertexInterface<T> beginVertex = vertices.get(begin);
+        VertexInterface<T> endVertex = vertices.get(end);
         if ((beginVertex != null) && (endVertex != null))
+        {
             result = beginVertex.connect(endVertex, edgeWeight);
+            edges[getSource(begin)][getEnd(end)] = true;
+        }
         if (result)
             edgeCount++;
         return result;
@@ -44,8 +51,8 @@ public class DirectedGraph<T> implements GraphInterface<T>
     public boolean hasEdge(T begin, T end)
     {
         boolean found = false;
-        VertexInterface<T> beginVertex = vertices.getValue(begin);
-        VertexInterface<T> endVertex = vertices.getValue(end);
+        VertexInterface<T> beginVertex = vertices.get(begin);
+        VertexInterface<T> endVertex = vertices.get(end);
         if ((beginVertex != null) && (endVertex != null))
         {
             Iterator<VertexInterface<T>> neighbors = beginVertex.getNeighborIterator();
@@ -66,7 +73,7 @@ public class DirectedGraph<T> implements GraphInterface<T>
 
     public int getNumberOfVertices()
     {
-        return vertices.getSize();
+        return vertices.size();
     }
 
     public int getNumberOfEdges()
@@ -82,7 +89,7 @@ public class DirectedGraph<T> implements GraphInterface<T>
 
     protected void resetVertices()
     {
-        Iterator<VertexInterface<T>> vertexIterator = vertices.getValueIterator();
+        Iterator<VertexInterface<T>> vertexIterator = vertices.values().iterator();
         while (vertexIterator.hasNext())
         {
             VertexInterface<T> nextVertex = vertexIterator.next();
@@ -97,7 +104,7 @@ public class DirectedGraph<T> implements GraphInterface<T>
         resetVertices();
         QueueInterface<T> traversalOrder = new LinkedQueue<T>();
         QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<VertexInterface<T>>();
-        VertexInterface<T> originVertex = vertices.getValue(origin);
+        VertexInterface<T> originVertex = vertices.get(origin);
         originVertex.visit();
         traversalOrder.enqueue(origin);
         vertexQueue.enqueue(originVertex);
@@ -124,7 +131,7 @@ public class DirectedGraph<T> implements GraphInterface<T>
         resetVertices();
         QueueInterface<T> traversalOrder = new LinkedQueue<>();
         StackInterface<VertexInterface<T>> vertexStack = new LinkedStack<>();
-        VertexInterface<T> originVertex = vertices.getValue(origin);
+        VertexInterface<T> originVertex = vertices.get(origin);
         originVertex.visit();
         traversalOrder.enqueue(origin);
         vertexStack.push(originVertex);
@@ -144,4 +151,86 @@ public class DirectedGraph<T> implements GraphInterface<T>
         return traversalOrder;
     }
 
+    public int getSource(T vertexLabel)
+    {
+        String label = (String) vertexLabel;
+        int source = 0;
+        switch(label)
+        {
+            case "A":
+                source = 0;
+                break;
+            case "B":
+                source = 1;
+                break;
+            case "C":
+                source = 2;
+                break;
+            case "D":
+                source = 3;
+                break;
+            case "E":
+                source = 4;
+                break;
+            case "F":
+                source = 5;
+                break;
+            case "G":
+                source = 6;
+                break;
+            case "H":
+                source = 7;
+                break;
+            case "I":
+                source = 8;
+                break;
+            default:
+                break;
+        }
+        return source;
+    }
+
+    public int getEnd(T vertexLabel)
+    {
+        String label = (String) vertexLabel;
+        int end = 0;
+        switch(label)
+        {
+            case "A":
+                end = 0;
+                break;
+            case "B":
+                end = 1;
+                break;
+            case "C":
+                end = 2;
+                break;
+            case "D":
+                end = 3;
+                break;
+            case "E":
+                end = 4;
+                break;
+            case "F":
+                end = 5;
+                break;
+            case "G":
+                end = 6;
+                break;
+            case "H":
+                end = 7;
+                break;
+            case "I":
+                end = 8;
+                break;
+            default:
+                break;
+        }
+        return end;
+    }
+
+    public boolean[][] getAdjacencyMatrix()
+    {
+        return this.edges;
+    }
 } // end DirectedGraph
